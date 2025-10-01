fad2d004cd0529c9dce56f82580441c008ce3460
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2025-10-01 19:22:06 +0900

    다국어 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:houseoftomorrow/src/service/theme_service.dart';
 import 'package:houseoftomorrow/src/view/shopping/shopping_view.dart';
+import 'package:houseoftomorrow/util/lang/generated/l10n.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -19,6 +21,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       title: 'House Of Tomorrow',
       theme: context.themeService.themeData,
       debugShowCheckedModeBanner: false,
